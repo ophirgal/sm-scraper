e12de6bf83d4450670a43fd41997147e2aeb3366
Jerry Qian <jerry@qian.ninja>
2021-04-22 02:45:00 -0400

    cleaned up server.py

--- src/dashboard/server.py
+++ src/dashboard/server.py
@@ -46,44 +46,8 @@ def getData():
         print("STALE REQ: ABORTING")
         return
 
-    # states = request.args.get('states')
-    # statesList = states.split(",")
-
     cur = conn.cursor()
 
-    if reqState != thisState:
-        print("STALE REQ: ABORTING")
-        return
-
-    # Get all rows for all states
-    data = []
-    dataAvg = []
-    i = 0
-
-    '''
-    for s in statesList:
-    if reqState != thisState:
-      print("STALE REQ: ABORTING")
-      return
-    i += 1
-
-    cur.execute("SELECT \"submission_date\",\"{}\" FROM public.us_states_covid WHERE \"state\" = '{}' AND ((CAST('{}' AS date) - CAST(submission_date AS date)) % '{}') = 0 AND \"submission_date\" >= '{}' AND \"submission_date\" <= '{}' ORDER BY \"submission_date\" ASC;".format(attribute, s, mx, interval, mn, mx))
-
-    res = cur.fetchall()
-    thisData = []
-    for d in res:
-      thisData.append({'date':d[0].strftime("%Y-%m-%d"), 'val':float(d[1])})
-    data.append(thisData)
-
-    cur.execute("SELECT AVG(\"{}\") FROM public.us_states_covid WHERE \"state\" = '{}' AND ((CAST('{}' AS date) - CAST(submission_date AS date)) % '{}') = 0 AND \"submission_date\" >= '{}' AND \"submission_date\" <= '{}';".format(attribute, s, mx, interval, mn, mx))
-    res = cur.fetchall()
-    dataAvg.append({'state':s, 'avg':float(res[0][0])})
-    '''
-    if reqState != thisState:
-        print("STALE REQ: ABORTING")
-        return
-
-    # prep data for delivery
     jsonData = {'data': ""}
     resp = Response(response=json.dumps(jsonData),
                     status=200, mimetype='application/json')
